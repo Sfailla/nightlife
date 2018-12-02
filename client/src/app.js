783c26d13f224d85a0e64c26ec29e300d89b801f
Stevo99 <Sfailla1983@gmail.com>
2018-12-02 13:18:58 -0500

    minor changes

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
