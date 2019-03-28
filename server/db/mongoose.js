c64af86db832592e280180640ed0aa5c40faf56a
Stevo99 <Sfailla1983@gmail.com>
2019-03-28 15:17:23 -0400

    fixed mongo connection error

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose
-	.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true })
+	.connect(process.env.MONGOOSE_URI, {
+		useNewUrlParser: true,
+		useFindAndModify: false,
+		useCreateIndex: true
+	})
 	.then(() => console.log('connection established to MLAB database'))
 	.catch(err =>
 		console.log(
-			`${err.name} \n there is an error with mongoose connect: ${err.code}`
+			`${err.name} \n there is an error with mongoose connect: ${err.code} ${err.errmsg}`
 		)
 	);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useFindAndModify', false); // change for production
 
 module.exports = mongoose;
