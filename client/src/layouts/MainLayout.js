0da09044fb6ecb05959eb055364eb156c8bf6d62
Stevo99 <Sfailla1983@gmail.com>
2019-05-22 15:04:28 -0400

    removed header from app so the ui in mobile-view will look better

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -1,13 +1,11 @@
 import React, { Fragment } from 'react';
 
-import Header from '../components/Header';
 import HomePage from '../pages/HomePage';
 
 export default class MainLayout extends React.Component {
 	render() {
 		return (
 			<Fragment>
-				<Header />
 				<HomePage />
 			</Fragment>
 		);
