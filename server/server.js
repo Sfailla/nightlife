cda572d7db520598eb6e165a6fc415efe6795ff1
Stevo99 <Sfailla1983@gmail.com>
2019-07-21 12:21:08 -0400

    minor changes for deployment

--- server/server.js
+++ server/server.js
@@ -32,6 +32,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 3001;
 
-app.listen(port, () => console.log(`express server running on port: ${process.env.PORT}`));
+app.listen(port, () => console.log(`express server running on port: ${port}`));
 
 module.exports = { app };
