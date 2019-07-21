6c6263613c5873eaddf2c5804d33eb6fb09da045
Stevo99 <Sfailla1983@gmail.com>
2019-07-21 15:06:14 -0400

    changes for deployment

--- server/server.js
+++ server/server.js
@@ -26,9 +26,6 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../public')));
 }
 
-let dpath = path.join(__dirname, '../public', 'index.html');
-console.log(dpath);
-
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
