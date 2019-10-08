a28bc022325e7137b59f9748b15ba3d3c472f049
Stevo99 <Sfailla1983@gmail.com>
2019-10-08 18:18:25 -0400

    cleaned up css in side-drawer component.  reorganized css

--- client/src/components/SideDrawer.jsx
+++ client/src/components/SideDrawer.jsx
@@ -14,39 +14,30 @@ const SideDrawer = props => {
 	return (
 		<div className={drawerClass}>
 			<div className="side-drawer__avatar-section">
-				<div className="side-drawer__profile-wrapper">
-					{props.isLoggedIn ? (
-						<ul>
-							<li>
-								<Avatar avatar={props.avatar} size={'8rem'} />
-							</li>
-							<li>
-								<Typography
-									addStyles={{ color: 'white' }}
-									headingTertiary={props.username}
-								/>
-							</li>
-							<li>
-								<LogoutButton
-									logout={props.logout}
-									addStyles={{ width: '12rem' }}
-									name="logout"
-								/>
-							</li>
-						</ul>
-					) : (
-						<div className="side-drawer__img-wrapper">
-							<div className="side-drawer__logo-wrapper">
-								<p>NIGHT</p>
-								<p>LIFE</p>
-							</div>
-							<img
-								className="side-drawer__img"
-								src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzFCT8vZwhI1Vfxfr3GMMSsV1EKuLAxYK_hfwx2knuwiYK-buW"
+				{props.isLoggedIn ? (
+					<ul>
+						<li>
+							<Avatar avatar={props.avatar} size={'8rem'} />
+						</li>
+						<li>
+							<Typography
+								addStyles={{ color: 'white' }}
+								headingTertiary={props.username}
 							/>
-						</div>
-					)}
-				</div>
+						</li>
+						<li>
+							<LogoutButton
+								logout={props.logout}
+								addStyles={{ width: '12rem' }}
+								name="logout"
+							/>
+						</li>
+					</ul>
+				) : (
+					<div className="side-drawer__avatar-section--no-auth">
+						<p>NIGHTLIFE</p>
+					</div>
+				)}
 			</div>
 			<div className="side-drawer__nav-section">
 				<MobileNavigation closeDrawer={props.handleCloseDrawer} />
