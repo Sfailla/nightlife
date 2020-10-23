38d038428194821d2bc953219cfa1dc9b0692ae2
Sfailla <Sfailla1983@gmail.com>
2020-10-23 10:15:54 -0400

    updated server config

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,14 @@
 // ENV VARIABLES
 
-let env = process.env.NODE_ENV || 'development' || 'production';
+let env = process.env.NODE_ENV || 'development';
 console.log('node_env === ', env);
 
-if (env === 'development' || env === 'test' || env === 'production') {
-	let config = require('./config.json');
-	let envConfig = config[env];
+// if (env === 'development' || env === 'test' || env === 'production') {}
+let config = require('./config.json');
+let envConfig = config[env];
 
-	Object.keys(envConfig).map(key => {
-		process.env[key] = envConfig[key];
-	});
-}
+Object.keys(envConfig).map(key => {
+	process.env[key] = envConfig[key];
+});
 
 module.exports = env;
